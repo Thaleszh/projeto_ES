c255530f9264161e66bd693c7770703d3314dfa9
RflRckr <rafael.r.rocker@gmail.com>
2017-04-05 10:55:51 -0300

    Add files via upload

--- Main.py
+++ Main.py
@@ -2,20 +2,12 @@ import Control
 import Vision
 
 while True:
-    x = int(Vision.entry("\n1 - Add Class\n2 - Search Class\n3 - Edit Class\n4 - Remove Class\n5 - Exit\n"))
+    x = int(Vision.entry("\n1 - Add Class\n2 - Search Class\n3 - Add Ability\n4 - Exit\n"))
     if x == 1:
         Control.addClass()
     elif x == 2:
         Control.searchTag()
     elif x == 3:
-        y = int(Vision.entry("  1 - Add Ability\n  2 - Change Class Name\n  3 - Duplicate Class\n"))
-        if y == 1:
-            Control.addAbility()
-        elif y == 2:
-            Control.changeName()
-        else:
-            Control.duplicateClass()
-    elif x == 4:
-        Control.removeCLass()
+        Control.addAbility()
     else:
         break
