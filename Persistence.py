f6c0ae442201a6170580756241246bf6cf502ce5
RflRckr <rafael.r.rocker@gmail.com>
2017-05-09 12:11:24 -0300

    Add files via upload

--- Persistence.py
+++ Persistence.py
@@ -1,5 +1,8 @@
 
 classes = list()
+characters = list()
+tables = list()
+openTables = list()
 
 def saveClass(classToAdd):
 	classe = getClass(classToAdd.getName())
@@ -14,8 +17,40 @@ def getClass(name):
 			return classInList
 	return "notFound"
 
-def removeClass(name):
+def delClass(name):
 	for classInList in classes:
 		if classInList.getName() == name:
 			classes.remove(classInList)
+			break
 
+def addOpenTable(table):
+	table = getTable(table.getName())
+	if table == "notFound":
+		openTables.append(table)
+	else:
+		openTables[openTables.index(table)] = table
+
+def delOpenTable(name):
+	for table in openTables:
+		if openTables.getName() == name:
+			openTables.remove(table)
+			break
+
+def getTable(name):
+	for table in tables:
+		if table.getName() == name:
+			return table
+	return "notFound"
+
+def saveTable(table):
+	table = getTable(table.getName())
+	if table == "notFound":
+		tables.append(table)
+	else:
+		tables[tables.index(table)] = table
+
+def delTable(name):
+	for table in tables:
+		if tables.getName() == name:
+			tables.remove(table)
+			break
