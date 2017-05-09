04e332906c3990892a718975c17838d35ab2c815
RflRckr <rafael.r.rocker@gmail.com>
2017-05-09 11:39:06 -0300

    Add files via upload

--- Control.py
+++ Control.py
@@ -1,4 +1,5 @@
 from Class import Class
+from Table import Table
 import Vision
 import Persistencia
 user = 1
@@ -37,7 +38,7 @@ def changeName():
     old = Vision.entry("Class to change name: ")
     new = Vision.entry("New Class Name: ")
     classe = Persistencia.getClass(old)
-    Persistencia.delClass(classe)
+    Persistencia.removeClass(classe)
     classe.setName(new)
     Persistencia.saveClass(classe)
 
@@ -48,6 +49,75 @@ def duplicateClass():
     classe.setName(new)
     Persistencia.saveClass(classe)
 
-def delClass():
+def removeCLass():
     classe = Vision.entry("Name of the class to be deleted: ")
-    Persistencia.delClass(classe)
+    Persistencia.saveClass(classe)
+
+def createTable():
+    name = Vision.entry("Table name: ")
+    newTable = Table(name, user)
+    Persistencia.saveTable(newTable)
+
+def delTable():
+    name = Vision.entry("Table name: ")
+    Persistencia.delTable(name)
+
+def editTable():
+    option = Vision.entry("\n1 - Add Character\n2 - Remove Character\n3 - Add PLayer\n4 - Remove Player\n5 - Quit\n")
+    cases[option]()
+
+cases = {1 : caseAddC, 2 : caseDelC, 3 : caseAddP, 4 : caseDelP, 5 : caseQuit}
+
+def caseAddC():
+    name = Vision.entry("Table name: ")
+    table = Persistencia.getTable(name)
+    playerName = Vision.entry("Player name: ")
+    player = Persistencia.getUser(playerName)
+    charName = Vision.entry("Character name: ")
+    char = player.getCharacter(charName)
+    table.addCharacter(user, char)
+
+def caseDelC():
+    name = Vision.entry("Table name: ")
+    table = Persistencia.getTable(name)
+    playerName = Vision.entry("Player name: ")
+    player = Persistencia.getUser(playerName)
+    charName = Vision.entry("Character name: ")
+    char = player.getCharacter(charName)
+    table.delCharacter(user, char)
+
+def caseAddP():
+    name = Vision.entry("Table name: ")
+    table = Persistencia.getTable(name)
+    playerName = Vision.entry("Player name: ")
+    player = Persistencia.getUser(playerName)
+    table.addPlayer(user, player)
+
+def caseDelP():
+    name = Vision.entry("Table name: ")
+    table = Persistencia.getTable(name)
+    playerName = Vision.entry("Player name: ")
+    player = Persistencia.getUser(playerName)
+    table.delPlayer(user, player)
+
+def caseQuit():
+    name = Vision.entry("Table name: ")
+    table = Persistencia.getTable(name)
+    table.quit(player)
+
+def editLog():
+    name = Vision.entry("Table name: ")
+    table = Persistencia.getTable(name)
+    option = Vision.entry("\n1 - Edit Lore\n2 - Edit Log\n")
+    if option == 1:
+        lore = table.getLore()
+        Vision.display(lore)
+        lore = Vision.entry("Type the new lore: ")
+        table.setLore(lore)
+        Persistencia.saveTable(table)
+    else:
+        log = table.getLog()
+        Vision.display(log)
+        log = Vision.entry("Type the new log: ")
+        table.setLore(log)
+        Persistencia.saveTable(table)
