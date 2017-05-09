85c2f47f41f89d2bf647b14265d2d19f97978570
RflRckr <rafael.r.rocker@gmail.com>
2017-05-09 10:42:49 -0300

    Add files via upload

--- User.py
+++ User.py
@@ -1,3 +1,16 @@
 class User:
-    name
-    password
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+
+    def getName():
+        return self.name
+
+    def setName(newName):
+        self.name = newName
+
+    def getPassword():
+        return self.password
+
+    def setPassword(newPass):
+        self.password = newPass
