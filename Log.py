cf259ca25800945aa3f443187c9c7fe581ea945c
RflRckr <rafael.r.rocker@gmail.com>
2017-05-09 10:14:42 -0300

    Add files via upload

---
+++ Log.py
@@ -0,0 +1,14 @@
+Description = ""
+Name = ""
+
+def getName():
+    return Name
+
+def setName(newName):
+    Name = newName
+
+def getDescription():
+    return Description
+
+def setDescription(newDescription):
+    Description = newDescription
